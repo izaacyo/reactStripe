27ccccf2065139fd4b2b25d5dc81414a6b272b1a
izaacyo <mariusppetrut@gmail.com>
2021-10-08 17:44:05 +0300

    stripeCheckout done

--- ReactAndRedux/reactStripe/reactstripe/src/Pay.jsx
+++ ReactAndRedux/reactStripe/reactstripe/src/Pay.jsx
@@ -1,21 +1,47 @@
 import StripeCheckout from "react-stripe-checkout"
+import {useState, useEffect} from "react"
+import axios from "axios"
+import {useHistory} from "react-router"
 
 const KEY = "pk_test_51JiIKUEBhQiQRacKjzJI9SRfzp7iPMrGx3lozF50rUbLUHHItTM2tS9R0WZpSKcrIm4lA2XV7d9UDNcPOCTLCK0C00qUJ6HFg7"
 
 const Pay = () => {
 
+    const [stripeToken, setStripeToken] = useState(null);
+    const history = useHistory()
+
 const onToken = (token) => {
-    console.log(token)
+    setStripeToken(token)
 }
+
+useEffect(() => {
+    const makeRequest = async() => {
+        try{
+const res = await axios.post("http://localhost:5000/api/checkout/payment", {
+tokenId: stripeToken.id,
+amount: 2000
+})
+console.log(res.data)
+history.push("/success")
+        }catch(err){
+            console.log(err)
+        }
+    }
+    stripeToken && makeRequest()
+}
+, [stripeToken, history])
+
 
     return(
 <div
 style={{
-    hegiht:"100vh",
+    height:"100vh",
     display:"flex",
     alignItems:"center",
     justifyContent:"center"
 }}>
+    {stripeToken ? (<span> Processing. Please wait...</span>) :
+    ( 
 <StripeCheckout
   name= "Izaac Shop" 
   image = "https://avatars.githubusercontent.com/u/1486366?v=4"
@@ -41,6 +67,7 @@ style= {{
 > Pay Now
 </button>
 </StripeCheckout>
+)}
 
 </div>
     )
